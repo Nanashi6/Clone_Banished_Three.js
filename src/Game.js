2cdb6b1cc5c26ed76bc11c801b2c0be980e7937c
Yury Evzhenko <yuevzhenko@gmail.com>
2024-03-31 15:18:26 +0300

    Added camera control and main scene

---
+++ src/Game.js
@@ -0,0 +1,67 @@
+import * as THREE from 'three';
+import { CameraManager } from 'cameraManager';
+
+export class Game {
+    gameWindow;
+    scene;
+  
+    cameraManager;
+    camera;
+  
+    renderer;
+  
+    gridSize;
+  
+    plane;
+  
+    grid;
+  
+    constructor(gridSize) {
+      this.gameWindow = document.getElementById('render-target');
+  
+      // Создаем сцену, камеру и рендерер
+      this.scene = new THREE.Scene();
+      this.scene.background = new THREE.Color(0x777777);
+  
+      this.cameraManager = new CameraManager(this.gameWindow);
+      this.camera = this.cameraManager.camera;
+  
+      this.renderer = new THREE.WebGLRenderer();
+      this.renderer.setSize(this.gameWindow.offsetWidth, this.gameWindow.offsetHeight);
+      this.gameWindow.appendChild(this.renderer.domElement);
+  
+      window.addEventListener('resize', this.resizeWindow, false);
+  
+      document.addEventListener('contextmenu', (event) => event.preventDefault(), false);
+      
+      this.gridSize = gridSize; // Размер карты
+  
+      this.plane = new THREE.Mesh(new THREE.PlaneGeometry(this.gridSize, this.gridSize, 1), new THREE.MeshBasicMaterial({color: 0x005500}));
+      this.plane.position.set(0, -1, 0);
+      this.plane.rotateX(-Math.PI / 2);
+      // Добавляем объект в сцену
+      this.scene.add(this.plane);
+      this.plane.visible = true;
+  
+      this.grid = new THREE.GridHelper(this.gridSize, this.gridSize);
+      this.grid.position.set(this.plane.position.x, this.plane.position.y, this.plane.position.z);
+      this.scene.add(this.grid);
+      // Скрываем отображение ячеек грида
+      this.grid.visible = true;
+  
+      this.renderer.setAnimationLoop(this.animate);
+    }
+  
+    animate = () => {
+      this.renderer.render(this.scene, this.camera);
+    }
+  
+    resizeWindow = () => {
+      this.camera.aspect = this.gameWindow.offsetWidth / this.gameWindow.offsetHeight;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(this.gameWindow.offsetWidth, this.gameWindow.offsetHeight);
+    }
+}
+window.onload = () => {
+    window.game = new Game(200);
+}
